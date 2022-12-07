63b9c7709b9f7ebcee1f5918c499e0df6dc7866d
Eddy Shao <dev@eddyshao.com>
2022-12-07 21:48:21 +1100

    add ranks to live info, displays solo queue rank if not in a ranked game mode

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 import json
 import requests
-import utils
+import asset_utils
 
 import os
 from dotenv import load_dotenv
@@ -63,41 +63,76 @@ async def live(ctx, summoner):
     with open("lol_constants/champion.json") as f:
         champs = json.load(f)["data"]
 
+    with open("lol_constants/queues.json") as f:
+        queues = json.load(f)
+
+    queue_dict = {
+        "Ranked Flex": "RANKED_FLEX_SR",
+        "Ranked Solo/Duo": "RANKED_SOLO_5x5",
+    }
+
     try:
+        queue_id = str(game_data["gameQueueConfigId"])
+        queue_data = queues[queue_id]
+        queue_name = queue_data["name"]
+        if queue_name == "Normal":
+            queue_name = queue_data["detailedDescription"]
+
         participants = game_data["participants"]
         for participant in participants:
             curr_name = participant["summonerName"]
+
+            curr_id = participant["summonerId"]
+            summoner_url = f"{RIOT_SERVICE_BASEURL}api/riot/lolBySummoner/{curr_id}"
+            r2 = requests.get(summoner_url, verify=False)
+            curr_summoner = json.loads(r2.json())
+            curr_division = ""
+            if not curr_summoner:
+                rank_emote = get_emote_str("UNRANKED")
+            else:
+                if "Normal" in queue_name or "ARAM" in queue_name:
+                    queue_name = "Ranked Solo/Duo"
+                curr_queue = next(
+                    queue for queue in curr_summoner if queue_dict[queue_name] == queue["queueType"])
+                rank_emote = get_emote_str(curr_queue["tier"])
+                curr_division = curr_queue["rank"]
             champ = next(champ for champ in champs if champs[champ]["key"] == str(
                 participant["championId"]))
-            champ_emote = next(
-                f"<:{emote.name}:{emote.id}>" for emote in gromp.emojis if emote.name == champ)
+            champ_emote = get_emote_str(champ)
             if participant["teamId"] == 100:
-                blue_team.append(f"{champ_emote} {curr_name}")
+                blue_team.append(
+                    f"{champ_emote} {curr_name} ({rank_emote} {curr_division})")
             else:
-                red_team.append(f"{champ_emote} {curr_name}")
-
+                red_team.append(
+                    f"{champ_emote} {curr_name} ({rank_emote} {curr_division})")
         map_name = [map for map in maps if map["mapId"]
                     == game_data["mapId"]][0]["mapName"]
         map_id = str(game_data["mapId"])
         game_mode = game_data["gameMode"]
-
     except KeyError:
         await ctx.interaction.followup.send(f"{summoner_name} is not in game.")
         return
 
-    msg = f"Currently playing {map_name}, {game_mode}"
+    msg = f"Currently playing {queue_data['name']}, {map_name}"
     live_deets = discord.Embed(title=f"{summoner_name}",
                                description=msg,
                                color=0xff00ff)
-    live_deets.set_thumbnail(url=utils.get_game_icon_path(map_id, game_mode))
+    live_deets.set_thumbnail(
+        url=asset_utils.get_game_icon_path(map_id, game_mode))
     live_deets.add_field(name="Blue Team",
                          value="\n".join(blue_team),
                          inline=True)
     live_deets.add_field(name="Red Team",
                          value="\n".join(red_team),
                          inline=True)
-    live_deets.set_image(url=utils.get_parties_bg_path(map_id, game_mode))
+    live_deets.set_image(
+        url=asset_utils.get_parties_bg_path(map_id, game_mode))
 
     await ctx.interaction.followup.send(embed=live_deets)
+
+def get_emote_str(emote_name):
+    return next(
+        f"<:{emote.name}:{emote.id}>" for emote in gromp.emojis if emote.name == emote_name)
+
 
 gromp.run(DISCORD_TOKEN)
