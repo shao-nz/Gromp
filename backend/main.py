1047ece489d47e16a1c7f244b2974983c89f23d5
Eddy Shao <dev@eddyshao.com>
2023-01-04 22:29:32 +1100

    add frontend

--- backend/main.py
+++ backend/main.py
@@ -95,8 +95,15 @@ async def live(ctx, summoner):
             else:
                 if "Normal" in queue_name or "ARAM" in queue_name:
                     queue_name = "Ranked Solo/Duo"
-                curr_queue = next(
-                    queue for queue in curr_summoner if lol_types.QUEUES[queue_name] == queue["queueType"])
+
+                print(curr_summoner)
+                if len(curr_summoner) == 1:
+                    curr_queue = curr_summoner[0]
+                    print(curr_queue)
+                else:
+                    curr_queue = next(
+                        queue for queue in curr_summoner if lol_types.QUEUES[queue_name] == queue["queueType"])
+                print(curr_queue["tier"])
                 rank_emote = get_emote_str(curr_queue["tier"])
                 curr_division = curr_queue["rank"]
 
